6ff508472c7d27b2c74c0668a1ff4b00533f501d
BIMASKUY <113542041+BIMASKUY@users.noreply.github.com>
2022-11-16 23:50:22 +0700

    Create D. Hapus Satu Saja!.cpp

---
+++ Perkenalan Pemrograman Kompetitif/D. Hapus Satu Saja!.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+vector<string> v;
+string S, Pembanding;
+cin >> S >> Pembanding;
+
+for(int i = 0; i < S.size(); i++){
+    string Kalimat = S;
+    v.push_back(Kalimat.erase(i,1));
+}
+
+bool test = false;
+
+for(int i = 0; i < v.size(); i++){
+    if(v[i] == Pembanding){
+        test = true;
+    }
+}
+
+if(test == true){
+    cout << "Tentu saja bisa!";
+}
+else{
+    cout << "Wah, tidak bisa :(";
+}
+
+}
